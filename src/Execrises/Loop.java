5d72180c0fd903398df8e69c9b3d550c7e786f9f
[Niggle Nandu] <nigglenandu@gmail.com>
2024-12-11 22:54:17 +0545

    loop added

---
+++ src/Execrises/Loop.java
@@ -0,0 +1,23 @@
+package Execrises;
+
+public class Loop {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+
+        int a = 1;
+        while (a <= 10) {
+            System.out.print(a);
+            a++;
+        }
+
+        int b = 1;
+        do {
+            System.out.println(b);
+            b++;
+        } while (b <= 10);
+
+    }
+}
