ff14ada6036fa7289b2ea7a68dd101f6aac0569d
[Niggle Nandu] <nigglenandu@gmail.com>
2024-12-11 22:47:54 +0545

    ternary operator added

--- src/Execrises/ConditionalStatement.java
+++ src/Execrises/ConditionalStatement.java
@@ -1,2 +1,37 @@
-package Execrises;public class ConditionalStatement {
+package Execrises;
+
+public class ConditionalStatement {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 15;
+
+//        if...else
+//        if(condition){.....} else {....}
+
+        if(a>b){
+            System.out.println("a is greater than b");
+        } else{
+            System.out.println("b is greater");
+        }
+
+
+//        else if
+//        if(condition){.....} else if(condition) {....} else{..}
+
+        if(a>b){
+            System.out.println("a is greater than b");
+        } else if(a<b) {
+            System.out.println("b is greater");
+        } else {
+            System.out.println("both are equal");
+        }
+
+
+//       Ternary operator
+//       variable = (condition) ? expressionTrue :  expressionFalse;
+        String greater  = (a>b)? "a is greater" : "b is greater";
+        System.out.println(greater);
+
+    }
 }
+
