381caa1c61a9d11e7933456eebf49894c4f5cd74
[Niggle Nandu] <nigglenandu@gmail.com>
2024-12-12 22:41:35 +0545

    method add

---
+++ src/Execrises/Method.java
@@ -0,0 +1,32 @@
+package Execrises;
+
+public class Method {
+
+    public String greeting(){
+        return "I do work and return string.";
+    }
+
+    public int sum(int a, int b){
+        return a+b;
+    }
+
+    public static String information(){
+        return "hey I belongs to class not instance coz I am static, same for all";
+    }
+
+    public static int add(int x, int y){
+        return x+y;
+    }
+    public static void main(String[] args){
+
+        Method method = new Method();
+
+        System.out.println(method.greeting());
+
+        System.out.println(method.sum(5,5));
+
+        System.out.println("calling static method: " + information());
+
+        System.out.println("calling static method: " + add(5, 5));
+    }
+}
