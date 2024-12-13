83260d38e35588311b10a261a90e11d903c21949
[Niggle Nandu] <nigglenandu@gmail.com>
2024-12-13 22:46:46 +0545

    array added

---
+++ src/Execrises/Array.java
@@ -0,0 +1,21 @@
+package Execrises;
+
+public class Array {
+        public static void main(String[] args) {
+            String[] names = {"Niggle", "Nandu", "John", "Kurt"};
+            System.out.println(names[0]);
+
+            for(int i = 0; i<=3; i++){
+                System.out.println(names[i]);
+            }
+
+//          for each loop
+            for(String i : names){
+                System.out.println(i);
+            }
+
+//          replacing new name
+            names[2] = "Harry";
+            System.out.println(names[2]);
+        }
+}
