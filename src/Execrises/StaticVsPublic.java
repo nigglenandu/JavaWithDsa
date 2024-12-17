f98f187b96edaa771f961bf65b783b9a03e87e88
[Niggle Nandu] <nigglenandu@gmail.com>
2024-12-17 22:10:51 +0545

    staticvspublic added

---
+++ src/Execrises/StaticVsPublic.java
@@ -0,0 +1,18 @@
+package Execrises;
+
+public class StaticVsPublic {
+    static void myStaticMethod(){
+        System.out.println("I am static method so i don't need obj.");
+    }
+
+    public void myPublicMethod(){
+        System.out.println("i am public so call me from object");
+    }
+
+    public static void main(String[] args){
+        myStaticMethod();
+
+        StaticVsPublic obj = new StaticVsPublic();
+        obj.myPublicMethod();
+    }
+}
