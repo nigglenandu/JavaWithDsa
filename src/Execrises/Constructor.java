f96091930e914cac294f81ba6cabdacd581c0aa6
[Niggle Nandu] <nigglenandu@gmail.com>
2024-12-17 22:37:48 +0545

    Construtor added

---
+++ src/Execrises/Constructor.java
@@ -0,0 +1,13 @@
+package Execrises;
+
+public class Constructor {
+    int x = 5;
+
+
+    public Constructor(){
+        System.out.println(x);
+    }
+    public static void main(String[] args){
+        Constructor cons = new Constructor();
+    }
+}
