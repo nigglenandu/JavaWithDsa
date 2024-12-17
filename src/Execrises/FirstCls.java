17e0e1642644f040121375e3cf57ca3ee0f3e65f
[Niggle Nandu] <nigglenandu@gmail.com>
2024-12-17 21:48:16 +0545

    class added

---
+++ src/Execrises/FirstCls.java
@@ -0,0 +1,17 @@
+package Execrises;
+
+public class FirstCls {
+    int x = 5;
+    int y = 4;
+
+}
+
+class SecondCls{
+    public static void main(String[] args){
+        FirstCls firstObj = new FirstCls();
+        System.out.println(firstObj.x + firstObj.y);
+
+        firstObj.x = 4;
+        System.out.println(firstObj.x);
+    }
+}
