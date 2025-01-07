6c875750b02c23a95a40f4361ab3f62c9e326ef1
[Niggle Nandu] <nigglenandu@gmail.com>
2025-01-07 22:49:02 +0545

    hashing

---
+++ src/CodeSheet/Hashing.java
@@ -0,0 +1,38 @@
+package javaclass;
+import java.util.HashSet;
+import java.util.Iterator;
+public class Hashing {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+
+        //size
+        System.out.println("size of set is" + set.size());
+        // print all elements
+        System.out.println(set);
+        //Search - contains
+        if(set.contains(1)){
+            System.out.println("set contains 1");
+        }
+        if(!set.contains(6)){
+            System.out.println("does not contain");
+        }
+
+        // delete
+        set.remove(1);
+        if(!set.contains(1)){
+            System.out.print("doesnot contain 1");
+        }
+
+        //Iterator
+        Iterator it = set.iterator();
+        //hasNext Next
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+}
