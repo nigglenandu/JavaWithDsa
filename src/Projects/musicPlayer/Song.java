435bc9032a6dff0661c7dffd1e6161199928cf35
[Niggle Nandu] <nigglenandu@gmail.com>
2025-01-11 23:55:54 +0545

    music player project

---
+++ src/Projects/musicPlayer/Song.java
@@ -0,0 +1,25 @@
+package javaProject.musicPlayer;
+public class Song{
+    String title;
+    double duration;
+
+    public Song(String name, double duration){
+        this.title = name;
+        this.duration = duration;
+    }
+    public Song(){
+
+    }
+    public String getTitle() {
+        return title;
+    }
+    public double getDuration() {
+        return duration;
+    }
+
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration + '}';
+    }
+}
