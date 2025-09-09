81799ce6dfd4fb223a6356a90c14884819152b92
[Niggle Nandu] <nigglenandu@gmail.com>
2025-09-09 23:56:08 +0545

    constructor commented

--- src/CodeSheet/Constructor.java
+++ src/CodeSheet/Constructor.java
@@ -1,14 +1,18 @@
 package CodeSheet;
 
 public class Constructor {
-    int x = 5;
+    int x = 5;   // instance variable with default value 5
 
+    // constructor: runs automatically when object is created
     public Constructor(int y){
         System.out.println("I am constructor " + y);
     }
+
     public static void main(String[] args){
+        // creating object, constructor will be called
         Constructor cons = new Constructor(10);
 
-        System.out.println(cons.x);
+        // printing value of instance variable x
+        System.out.println(cons.x);  // Output: 5
     }
 }
