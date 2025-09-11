866bac110575d6dae12a1d68ae49fef7b70b3129
[Niggle Nandu] <nigglenandu@gmail.com>
2025-09-11 23:16:01 +0545

    custom gen arr

--- src/CodeSheet/CustomGenArrayList.java
+++ src/CodeSheet/CustomGenArrayList.java
@@ -3,16 +3,17 @@ package CodeSheet;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class CustomGenArrayList<T> {
+public class CustomGenArrayList<T> {   // generic class with type parameter T
 
-    private Object[] data;
+    private Object[] data;             // internal array (stores elements as Object)
     private static int DEFAULT_SIZE = 10;
-    private int size = 0;
+    private int size = 0;              // number of elements
 
     public CustomGenArrayList() {
-        data = new Object[DEFAULT_SIZE];
+        data = new Object[DEFAULT_SIZE]; // create array with default size
     }
 
+    // add element of type T
     public void add(T num) {
         if(isFull()) {
             resize();
@@ -20,47 +21,59 @@ public class CustomGenArrayList<T> {
         data[size++] = num;
     }
 
+    // double the size of array when full
     private void resize() {
         Object[] temp = new Object[data.length * 2];
-
-        for(int i =0; i<data.length; i++){
+        for(int i = 0; i < data.length; i++){
             temp[i] = data[i];
         }
         data = temp;
     }
 
+    // check if array is full
     private boolean isFull() {
         return size == data.length;
     }
 
+    // remove last element and return it
     public T remove() {
-        T removed = (T)(data[--size]);
+        T removed = (T)(data[--size]);  // type cast Object to T
         return removed;
     }
 
+    // get element at index
     public T get(int index) {
-        return (T)data[index];
+        return (T)data[index];          // type cast Object to T
     }
 
+    // return current size
     public int size() {
         return size;
     }
 
+    // replace value at index
     public void set(int index, T value){
         data[index] = value;
     }
 
+    // print array contents
     public String toString() {
         return "CustomGenArrayList{" + "data=" +
-                Arrays.toString(data) + ", size=" +size + '}';
+                Arrays.toString(data) + ", size=" + size + '}';
     }
+
     public static void main(String[] args) {
+        // Using without generics (raw type) - not type safe
         CustomGenArrayList list = new CustomGenArrayList();
+
+        // Normal Java ArrayList
         ArrayList<Integer> list2 = new ArrayList<>();
-     /*  CustomGenArrayList<Integer> list3 =new CustomGetArrayList<>();
-        for(int i =0; i<14; i++){
-            list3.add(2*i);
+
+        // Using our generic CustomGenArrayList
+        CustomGenArrayList<Integer> list3 = new CustomGenArrayList<>();
+        for(int i = 0; i < 14; i++){
+            list3.add(2 * i);   // add even numbers
         }
-        System.out.println(list3);*/
+        System.out.println(list3);
     }
 }
