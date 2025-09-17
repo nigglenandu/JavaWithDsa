fd92f1962638fde7378114f17a09c8ef1c74e7fa
[Niggle Nandu] <nigglenandu@gmail.com>
2025-09-17 22:44:44 +0545

    string comment

--- src/CodeSheet/StringTopic.java
+++ src/CodeSheet/StringTopic.java
@@ -3,28 +3,38 @@ package CodeSheet;
 public class StringTopic {
     public static void main(String[] args) {
 
+        // Creating a String
         String string = "NiggleNandu";
         System.out.println(string);
 
+        // length() → returns number of characters
         int length = string.length();
         System.out.println("length= " + length);
 
+        // toLowerCase() → converts to lowercase
         String lowerCase = string.toLowerCase();
         System.out.println("lowercase= " + lowerCase);
 
+        // toUpperCase() → converts to uppercase
         String upperCase = string.toUpperCase();
         System.out.println("UpperCase= " + upperCase);
 
+        // indexOf() → finds index of first occurrence of a character
         int index = string.indexOf("a");
         System.out.println("Index= " + index);
 
+        // Concatenation of Strings
         String firstName = "Niggle";
         String secondName = "Nandu";
+
+        // Using + operator
         System.out.println("Concat= " + firstName + secondName);
+
+        // Using concat() method
         System.out.println("concat= " + firstName.concat(secondName));
 
-        System.out.println(" \" \' \\ ");
-        System.out.println("my name \nis \t niggle");
-
+        // Escape characters in Strings
+        System.out.println(" \" \' \\ ");   // prints: " ' \
+        System.out.println("my name \nis \t niggle");  // newline and tab
     }
 }
