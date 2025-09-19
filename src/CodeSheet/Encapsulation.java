de2d7b44dd3d9a95800936256dfca676f2c0fd9c
[Niggle Nandu] <nigglenandu@gmail.com>
2025-09-19 22:07:23 +0545

    Encapsulation

--- src/CodeSheet/Encapsulation.java
+++ src/CodeSheet/Encapsulation.java
@@ -1,21 +1,30 @@
 package CodeSheet;
 
+// Encapsulation → wrapping variables (data) with methods (getters & setters)
 public class Encapsulation {
+    // private variable (cannot be accessed directly outside class)
     private String name;
 
+    // Getter → used to access private variable
     public String getName(){
         return name;
     }
 
+    // Setter → used to modify private variable
     public void setName(String name){
         this.name = name;
     }
 }
 
-class Main{
+// Main class to test encapsulation
+class Main {
     public static void main(String[] args){
         Encapsulation encap = new Encapsulation();
+
+        // Setting value using setter
         encap.setName("niggle");
+
+        // Getting value using getter
         System.out.println(encap.getName());
     }
 }
